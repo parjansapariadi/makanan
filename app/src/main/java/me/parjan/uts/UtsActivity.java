12ac607da0aff09012bb53197fdcf2ecdbb3cb2a
Parjan <parjaan7@gmail.com>
2021-06-10 08:11:15 -0700

    first commit

---
+++ app/src/main/java/me/parjan/uts/UtsActivity.java
@@ -0,0 +1,14 @@
+package me.parjan.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UtsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uts);
+    }
+}
